86192fc1e142562d38a9193f922e29fb7714b7ad
cerebralstoic <vineet.rawat.ddun62019@gmail.com>
2025-09-28 01:18:15 +0530

    Filled DataSource temporary

---
+++ src/components/DataSource/datasources.jsx
@@ -0,0 +1,15 @@
+const DataSources = () => {
+  return (
+    <div className="flex min-h-screen w-full items-center justify-center px-4">
+      <div className="mx-auto w-full max-w-2xl text-center pb-20">
+        <h1 className="text-pretty text-4xl font-semibold text-primary text-cyan-azure">
+          Data Sources
+        </h1>
+        <p className="mt-2 text-m text-muted-foreground text-dark-gray">
+          To access Data Sources you must have an account. Please sign in.
+        </p>
+      </div>
+    </div>
+  );
+};
+export default DataSources;
