0c866e4aa57460597d76d39109093ab9024070ea
Ankur Pandey <ANKURPANDEY.230112829@gehu.ac.in>
2025-09-28 16:09:53 +0530

    updated

--- src/services/visualizationengine.jsx
+++ src/services/visualizationengine.jsx
@@ -1,9 +1,13 @@
- import React from 'react'
- 
- const visualizationengine = () => {
-   return (
-     <div>visualizationengine</div>
-   )
- }
- 
- export default visualizationengine
+import { mockVisualizationData } from "../utils/mockData";
+
+/**
+ * Fetches all necessary data for the main Visualization dashboard (Parameter Correlations).
+ * * @returns {Promise<object>} The dashboard data (simulating an API response).
+ */
+export const fetchParameterCorrelationsData = async () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(mockVisualizationData);
+    }, 500);
+  });
+};
