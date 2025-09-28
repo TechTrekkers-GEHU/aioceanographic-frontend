6a68a11fb72ef98e60c7eca988c18c2be1106d92
Ankur Pandey <ANKURPANDEY.230112829@gehu.ac.in>
2025-09-28 16:08:56 +0530

    fixed opensidebar logic

--- src/components/SideBar/oceansidebar.jsx
+++ src/components/SideBar/oceansidebar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./oceansidebar.css";
 import { BiSolidThermometer } from "react-icons/bi";
 import { GiFishEggs, GiDna2 } from "react-icons/gi";
@@ -7,151 +7,146 @@ import { TbMapPin } from "react-icons/tb";
 import { FaFish, FaGlobe, FaMapMarkedAlt } from "react-icons/fa";
 import { LuActivity } from "react-icons/lu";
 
-const OceanSidebar = ({ isOpen }) => {
-  const [activeItem, setActiveItem] = useState(null);
 
-  const handleSelect = (id) => {
-    setActiveItem(id);
-  };
+const OceanSidebar = ({ activeItem, onSelect }) => {
+  
 
   return (
-    <aside className={`ocean-sidebar ${isOpen ? "open" : "closed"}`}>
-      {isOpen && (
-        <div>
-          {/* Ocean Parameters */}
-          <div className="section">
-            <h3 className="section-title">
-              <LuActivity /> Ocean Parameters
-            </h3>
-            <ul>
-              <li>
-                <button
-                  className={activeItem === "param" ? "active" : ""}
-                  onClick={() => handleSelect("param")}
-                >
-                  <div className="item-title">
-                    <span className="icon-box"><BiSolidThermometer /></span>
-                    Parameter Correlations
-                  </div>
-                  <div className="item-description">Temperature, salinity, pH</div>
-                </button>
-              </li>
-              <li>
-                <button
-                  className={activeItem === "biodiversity" ? "active" : ""}
-                  onClick={() => handleSelect("biodiversity")}
-                >
-                  <div className="item-title">
-                    <span className="icon-box"><FaFish /></span>
-                    Biodiversity Impact
-                  </div>
-                  <div className="item-description">Species distribution analysis</div>
-                </button>
-              </li>
-            </ul>
-          </div>
-
-          {/* Species Analysis */}
-          <div className="section">
-            <h3 className="section-title">
-              <GiFishEggs /> Species Analysis
-            </h3>
-            <ul>
-              <li>
-                <button
-                  className={activeItem === "otolith" ? "active" : ""}
-                  onClick={() => handleSelect("otolith")}
-                >
-                  <div className="item-title">
-                    <span className="icon-box"><GiFishEggs /></span>
-                    Otolith Analysis
-                  </div>
-                  <div className="item-description">Shape & morphometrics</div>
-                </button>
-              </li>
-              <li>
-                <button
-                  className={activeItem === "taxonomy" ? "active" : ""}
-                  onClick={() => handleSelect("taxonomy")}
-                >
-                  <div className="item-title">
-                    <span className="icon-box"><MdOutlineCategory /></span>
-                    Taxonomic Classification
-                  </div>
-                  <div className="item-description">Species identification</div>
-                </button>
-              </li>
-            </ul>
-          </div>
-
-          {/* Molecular Data */}
-          <div className="section">
-            <h3 className="section-title">
-              <GiDna2 /> Molecular Data
-            </h3>
-            <ul>
-              <li>
-                <button
-                  className={activeItem === "edna" ? "active" : ""}
-                  onClick={() => handleSelect("edna")}
-                >
-                  <div className="item-title">
-                    <span className="icon-box"><GiDna2 /></span>
-                    eDNA Analysis
-                  </div>
-                  <div className="item-description">Sequence matching</div>
-                </button>
-              </li>
-              <li>
-                <button
-                  className={activeItem === "genetic" ? "active" : ""}
-                  onClick={() => handleSelect("genetic")}
-                >
-                  <div className="item-title">
-                    <span className="icon-box"><GiDna2 /></span>
-                    Genetic Markers
-                  </div>
-                  <div className="item-description">Population genetics</div>
-                </button>
-              </li>
-            </ul>
-          </div>
-
-          {/* Spatial Analysis */}
-          <div className="section">
-            <h3 className="section-title">
-              <FaMapMarkedAlt /> Spatial Analysis
-            </h3>
-            <ul>
-              <li>
-                <button
-                  className={activeItem === "distribution" ? "active" : ""}
-                  onClick={() => handleSelect("distribution")}
-                >
-                  <div className="item-title">
-                    <span className="icon-box"><FaGlobe /></span>
-                    Species Distribution
-                  </div>
-                  <div className="item-description">Geospatial mapping</div>
-                </button>
-              </li>
-              <li>
-                <button
-                  className={activeItem === "hotspots" ? "active" : ""}
-                  onClick={() => handleSelect("hotspots")}
-                >
-                  <div className="item-title">
-                    <span className="icon-box"><TbMapPin /></span>
-                    Biodiversity Hotspots
-                  </div>
-                  <div className="item-description">Conservation priority areas</div>
-                </button>
-              </li>
-            </ul>
-          </div>
+    <div className="ocean-sidebar open">
+      <div>
+        {/* Ocean Parameters */}
+        <div className="section">
+          <h3 className="section-title">
+            <LuActivity /> Ocean Parameters
+          </h3>
+          <ul>
+            <li>
+              <button
+                className={activeItem === "param" ? "active" : ""}
+                onClick={() => onSelect("param")}
+              >
+                <div className="item-title">
+                  <span className="icon-box"><BiSolidThermometer /></span>
+                  Parameter Correlations
+                </div>
+                <div className="item-description">Temperature, salinity, pH</div>
+              </button>
+            </li>
+            <li>
+              <button
+                className={activeItem === "biodiversity" ? "active" : ""}
+                onClick={() => onSelect("biodiversity")}
+              >
+                <div className="item-title">
+                  <span className="icon-box"><FaFish /></span>
+                  Biodiversity Impact
+                </div>
+                <div className="item-description">Species distribution analysis</div>
+              </button>
+            </li>
+          </ul>
         </div>
-      )}
-    </aside>
+
+        {/* Species Analysis */}
+        <div className="section">
+          <h3 className="section-title">
+            <GiFishEggs /> Species Analysis
+          </h3>
+          <ul>
+            <li>
+              <button
+                className={activeItem === "otolith" ? "active" : ""}
+                onClick={() => onSelect("otolith")}
+              >
+                <div className="item-title">
+                  <span className="icon-box"><GiFishEggs /></span>
+                  Otolith Analysis
+                </div>
+                <div className="item-description">Shape & morphometrics</div>
+              </button>
+            </li>
+            <li>
+              <button
+                className={activeItem === "taxonomy" ? "active" : ""}
+                onClick={() => onSelect("taxonomy")}
+              >
+                <div className="item-title">
+                  <span className="icon-box"><MdOutlineCategory /></span>
+                  Taxonomic Classification
+                </div>
+                <div className="item-description">Species identification</div>
+              </button>
+            </li>
+          </ul>
+        </div>
+
+        
+        <div className="section">
+          <h3 className="section-title">
+            <GiDna2 /> Molecular Data
+          </h3>
+          <ul>
+            <li>
+              <button
+                className={activeItem === 'edna' ? 'active' : ''}
+                onClick={() => onSelect('edna')}
+              >
+                <div className="item-title">
+                  <span className="icon-box"><GiDna2 /></span>
+                  eDNA Analysis
+                </div>
+                <div className="item-description">Sequence matching</div>
+              </button>
+            </li>
+            <li>
+              <button
+                className={activeItem === 'genetic' ? 'active' : ''}
+                onClick={() => onSelect('genetic')}
+              >
+                <div className="item-title">
+                  <span className="icon-box"><GiDna2 /></span>
+                  Genetic Markers
+                </div>
+                <div className="item-description">Population genetics</div>
+              </button>
+            </li>
+          </ul>
+        </div>
+        {/* Spatial Analysis */}
+        <div className="section">
+          <h3 className="section-title">
+            <FaMapMarkedAlt /> Spatial Analysis
+          </h3>
+          <ul>
+            <li>
+              <button
+                className={activeItem === 'distribution' ? 'active' : ''}
+                onClick={() => onSelect('distribution')}
+              >
+                <div className="item-title">
+                  <span className="icon-box"><FaGlobe /></span>
+                  Species Distribution
+                </div>
+                <div className="item-description">Geospatial mapping</div>
+              </button>
+            </li>
+            <li>
+              <button
+                className={activeItem === 'hotspots' ? 'active' : ''}
+                onClick={() => onSelect('hotspots')}
+              >
+                <div className="item-title">
+                  <span className="icon-box"><TbMapPin /></span>
+                  Biodiversity Hotspots
+                </div>
+                <div className="item-description">Conservation priority areas</div>
+              </button>
+            </li>
+          </ul>
+        </div>
+
+      </div>
+    </div>
   );
 };
 
